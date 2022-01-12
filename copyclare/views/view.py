f038f924e3b032b4f076883a44ae6b4315a85f8e
adi <bozzhanov.at@gmail.com>
2022-01-12 15:17:37 +0000

    feat: rough draft of a view interface

--- copyclare/views/view.py
+++ copyclare/views/view.py
@@ -6,7 +6,7 @@ from ui.compiled.main_window import Ui_MainWindow
 from pages.home import HomePage
 
 
-class MainWindow(QMainWindow):
+class View(QMainWindow):
 
     pages = {
         "home": HomePage,
@@ -17,17 +17,46 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+    def get_pages(self):
+        """
+        Spits out a set of pages.
+
+        Returns:
+            :obj:`set`
+        """
+        return self.pages.keys()
+
     def load_page(self, page="home"):
-        # add the page to the main frame
+        """
+        Loads a page given the page name
+
+        Parameters:
+            page-(:obj:`str`)
+        """
         new_page = self.pages[page](self)
         new_page.show()
+
+    def update(self):
+        """
+        Updates the existing page with new information
+
+        """
+
+        pass
+
+    def _clear_page(self):
+        """
+        Clears the page from all widgets
+        """
+
+        pass
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    MainWindow = MainWindow()
-    MainWindow.show()
+    View = View()
+    View.show()
 
-    MainWindow.load_page("home")
+    View.load_page("home")
 
     sys.exit(app.exec())
