04faec0b76f611e90f819630e6a8e56d725c9e67
Adi <bozzhanov.at@gmail.com>
2022-03-09 16:33:14 +0000

    feat: banner

--- copyclare/widgets/banner.py
+++ copyclare/widgets/banner.py
@@ -10,10 +10,3 @@ class BannerWidget(QFrame):
         self.ui = load_ui("banner")
         self.ui.setupUi(self)
         self.ui.category_title.setText(title)
-
-        for i in range(10):
-            title, description, duration, img_path = map(
-                str, (i, i + 1, i + 2, i + 4))
-            _card = VideoCardWidget(self.ui.scrollArea, title, description,
-                                    duration, img_path)
-            self.ui.horizontalLayout.addWidget(_card)
