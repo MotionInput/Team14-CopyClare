2f4d776c3c5aaacedae912bc598a01e1981c1282
climax-form <zcabtc0@ucl.ac.uk>
2022-03-17 07:34:29 +0000

    improve video addition part

--- copyclare/pages/video_addition.py
+++ copyclare/pages/video_addition.py
@@ -1,13 +1,16 @@
 from PySide6.QtCore import Qt
 
-from copyclare.widgets.progress_banner import ProgressBannerWidget
 
 from .page import Page
-
 
 class Video_Addition(Page):
     def __init__(self, master):
         super().__init__(master, "ADD VIDEO")
+
+        video_name = self.ui.video_name_editor.getText()
+        video_description = self.ui.description_editor.getText()
+        video_tags = self.ui.tags_editor.getText()
+
+        self.ui.back_button.clicked.connect(lambda x: self.app.load_page("home"))
+
         
-        _banner = ProgressBannerWidget(self)
-        self.ui.verticalLayout.insertWidget(0, _banner)
