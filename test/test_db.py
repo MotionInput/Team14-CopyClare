1074925306bc9a26a726360033d2ff63eb25eff7
Sree S <sanakkayala12@gmail.com>
2022-03-30 17:22:57 +0100

    test stuff

---
+++ test/test_db.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+
+class DB_Testing(TestCase):
+
+    def test_something(self):
+        self.assertEqual(True, True)
+
+    def test_something1(self):
+        self.assertEqual(True, True)
