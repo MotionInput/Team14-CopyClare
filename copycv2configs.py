e5a407d46c4fe161c1c79d65c036c137e02e9a25
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2022-06-20 18:26:58 +0100

    Copy mediapipe modules

--- copycv2configs.py
+++ copycv2configs.py
@@ -3,6 +3,8 @@ import cv2
 import pathlib
 import re
 import os
+import mediapipe
+from distutils.dir_util import copy_tree
 
 cv2dir = pathlib.Path(cv2.__file__).parent
 
@@ -10,3 +12,7 @@ for file in os.listdir(cv2dir):
     if re.search(r"config*.py", file):
         print(f"Copying {file} to cv2 directory")
         shutil.copyfile(cv2dir.joinpath(file), f"build/copyclare/run.app/cv2/{file}")
+
+mediapipedir = pathlib.Path(mediapipe.__file__).parent
+
+copy_tree(mediapipedir.joinpath("modules"), "build/copyclare/run.app/mediapipe/modules")
