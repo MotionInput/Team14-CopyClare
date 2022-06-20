df58c791abb215c76fc84fe93204fe31331168de
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2022-06-20 10:37:02 +0100

    Fix directory

--- deploy_macos.sh
+++ deploy_macos.sh
@@ -1,6 +1,6 @@
 python update_mediapipe_so.py
-python -m nuitka --standalone --lto=no --assume-yes-for-downloads --plugin-enable=numpy --plugin-enable=pyside6 --disable-plugin=anti-bloat --output-dir=build/copyclare run.py
+python -m nuitka --standalone --lto=no --assume-yes-for-downloads --plugin-enable=numpy --plugin-enable=pyside6 --disable-plugin=anti-bloat --include-module=numpy.core.multiarray --output-dir=build/copyclare run.py
 
+mv build/copyclare/run.dist build/copyclare/run.app
 mkdir build/copyclare/run.dist/data
 cp -r  data_clean/* build/copyclare/run.dist/data
-mv build/copyclare/run.dist /build/copyclare/run.app
